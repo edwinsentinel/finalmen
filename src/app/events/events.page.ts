09cb271b2839146dcba746197ad50c7014faf27b
Edwin <waweruedwin2@gmail.com>
2019-03-27 14:01:33 +0300

    events

--- src/app/events/events.page.ts
+++ src/app/events/events.page.ts
@@ -6,9 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.page.scss'],
 })
 export class EventsPage implements OnInit {
-
-  constructor() { }
-
+  private selectedItem: any;
+  private icons = [
+    'flask',
+    'wifi',
+    'beer',
+    'football',
+    'basketball',
+    'paper-plane',
+    'american-football',
+    'boat',
+    'bluetooth',
+    'build'
+  ];
+  public items: Array<{ title: string; note: string; icon: string }> = [];
+  constructor() {
+    for (let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Event' + i,
+        note: 'This is EVENT#' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+    }
+  }
   ngOnInit() {
   }
 
