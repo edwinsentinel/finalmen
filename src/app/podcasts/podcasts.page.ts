6a451697471f9e968fc2564ffd32b33c8a198aa0
Edwin <waweruedwin2@gmail.com>
2019-04-02 10:52:15 +0300

    pdffile

--- src/app/podcasts/podcasts.page.ts
+++ src/app/podcasts/podcasts.page.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
 import { NavController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
+import { File } from '@ionic-native/File/ngx';
 
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer/ngx';
+import { FileTransfer } from '@ionic-native/file-transfer/ngx';
 @Component({
   selector: 'app-podcasts',
   templateUrl: './podcasts.page.html',
@@ -10,12 +15,54 @@ import { NavController } from '@ionic/angular';
 export class PodcastsPage implements OnInit {
 vid = 'https://www.youtube.com/embed/wyVM1evRxNw';
   data: { 'heading': string; 'Date': string; 'Activity': string; };
-  constructor(public navctrl: NavController, private dom: DomSanitizer) {
+  constructor(public navctrl: NavController, private dom: DomSanitizer, private platform: Platform, private file: File,
+// tslint:disable-next-line: deprecation
+    private ft: FileTransfer, private fileOpener: FileOpener, private document: DocumentViewer, ) {
 
   }
 sanitize(vid) {
 return this.dom.bypassSecurityTrustResourceUrl(vid);
 }
+// local pdf
+openLocalPdf() {
+const filePath = this.file.applicationDirectory + 'www/assets';
+if (this.platform.is('android')) {
+  const fakeName = Date.now();
+  this.file.copyFile(filePath, 'nat.pdf', this.file.dataDirectory, `${fakeName}.pdf`).then(result => {
+    this.fileOpener.open(result.nativeURL, 'application/pdf')
+      .then(() => console.log('File is opened'))
+      .catch(e => console.log('Error opening file', e));
+  });
+} else {
+  // Use Document viewer for iOS for a better UI
+  const options: DocumentViewerOptions = {
+    title: 'My PDF'
+  };
+  this.document.viewDocument(`${filePath}/nat.pdf`, 'application/pdf', options);
+}
+}
+// download pdf
+downloadAndOpenPdf() {
+  const downloadUrl = 'https://devdactic.com/html/5-simple-hacks-LBT.pdf';
+  const path = this.file.dataDirectory;
+  const transfer = this.ft.create();
+
+  transfer.download(downloadUrl, path + 'myfile.pdf').then(entry => {
+    const url = entry.toURL();
+
+    if (this.platform.is('ios')) {
+      this.document.viewDocument(url, 'application/pdf', {});
+    } else {
+      this.fileOpener.open(url, 'application/pdf')
+        .then(() => console.log('File is opened'))
+        .catch(e => console.log('Error opening file', e));
+    }
+  });
+}
+
+
+
+
 ngOnInit() {
   setTimeout(() => {
     this.data = {
